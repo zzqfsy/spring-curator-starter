5c5a690f49d40b2576201772f828872cbc9426f9
zzqfsy <zzqfsy@126.com>
2017-10-10 00:09:08 +0800

    opt

---
+++ zzqfsy-curator-test/src/main/java/com/zzqfsy/curator/test/discover/ServiceDetail.java
@@ -0,0 +1,30 @@
+package com.zzqfsy.curator.test.discover;
+
+public class ServiceDetail {
+    public static final String REGISTER_ROOT_PATH = "/service";
+
+    private String desc;
+    private int weight;
+
+    public ServiceDetail() {}
+    public ServiceDetail(String desc, int weight) {
+        this.desc = desc;
+        this.weight = weight;
+    }
+    public String getDesc() {
+        return desc;
+    }
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+    public int getWeight() {
+        return weight;
+    }
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+    @Override
+    public String toString() {
+        return "ServiceDetail [desc=" + desc + ", weight=" + weight + "]";
+    }
+}
